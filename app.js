fd8dfe4c873481aeaf1e93ffe97020baca424cd7
aditya <adityamahajan750@gmail.com>
2026-01-21 13:40:16 +0530

    invoice id removed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3000/api/invoices/1")
+fetch("http://localhost:3000/api/invoices/")
     .then(res => res.json())
     .then(invoices => {
         const output = document.getElementById("output");
